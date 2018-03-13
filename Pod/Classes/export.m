f2e255a29480dad2d552fb783ba72a1a845e35c2
Julesy <yan.outside@gmail.com>
2018-03-13 19:34:32 +0800

    测试2

---
+++ Pod/Classes/export.m
@@ -0,0 +1,125 @@
+{
+  "random" : false,
+  "authPass" : null,
+  "useOnlinePac" : false,
+  "TTL" : 0,
+  "global" : false,
+  "reconnectTimes" : 3,
+  "index" : 0,
+  "proxyType" : 0,
+  "proxyHost" : null,
+  "authUser" : null,
+  "proxyAuthPass" : null,
+  "isDefault" : false,
+  "pacUrl" : null,
+  "configs" : [
+    {
+      "enable" : true,
+      "password" : "EDtJkX3FkNUanu5",
+      "method" : "chacha20-ietf-poly1305",
+      "remarks" : "美国0.2",
+      "server" : "us1-sta87.ywp7d.pw",
+      "kcptun" : {
+        "nocomp" : false,
+        "key" : "it's a secrect",
+        "crypt" : "aes",
+        "datashard" : 10,
+        "mtu" : 1350,
+        "mode" : "fast",
+        "parityshard" : 3,
+        "arguments" : ""
+      },
+      "enabled_kcptun" : false,
+      "server_port" : 17346,
+      "remarks_base64" : "576O5Zu9MC4y"
+    },
+    {
+      "enable" : true,
+      "password" : "EDtJkX3FkNUanu5",
+      "method" : "chacha20-ietf-poly1305",
+      "remarks" : "香港0.2",
+      "server" : "hk3-sta87.ywp7d.pw",
+      "kcptun" : {
+        "nocomp" : false,
+        "key" : "it's a secrect",
+        "crypt" : "aes",
+        "datashard" : 10,
+        "mtu" : 1350,
+        "mode" : "fast",
+        "parityshard" : 3,
+        "arguments" : ""
+      },
+      "enabled_kcptun" : false,
+      "server_port" : 17346,
+      "remarks_base64" : "6aaZ5rivMC4y"
+    },
+    {
+      "enable" : true,
+      "password" : "EDtJkX3FkNUanu5",
+      "method" : "chacha20-ietf-poly1305",
+      "remarks" : "美国0.4",
+      "server" : "us2-sta87.ywp7d.pw",
+      "kcptun" : {
+        "nocomp" : false,
+        "key" : "it's a secrect",
+        "crypt" : "aes",
+        "datashard" : 10,
+        "mtu" : 1350,
+        "mode" : "fast",
+        "parityshard" : 3,
+        "arguments" : ""
+      },
+      "enabled_kcptun" : false,
+      "server_port" : 17346,
+      "remarks_base64" : "576O5Zu9MC40"
+    },
+    {
+      "enable" : true,
+      "password" : "EDtJkX3FkNUanu5",
+      "method" : "chacha20-ietf-poly1305",
+      "remarks" : "香港1.0",
+      "server" : "hk1-sta87.ywp7d.pw",
+      "kcptun" : {
+        "nocomp" : false,
+        "key" : "it's a secrect",
+        "crypt" : "aes",
+        "datashard" : 10,
+        "mtu" : 1350,
+        "mode" : "fast",
+        "parityshard" : 3,
+        "arguments" : ""
+      },
+      "enabled_kcptun" : false,
+      "server_port" : 17346,
+      "remarks_base64" : "6aaZ5rivMS4w"
+    },
+    {
+      "enable" : true,
+      "password" : "EDtJkX3FkNUanu5",
+      "method" : "chacha20-ietf-poly1305",
+      "remarks" : "日本1.5",
+      "server" : "jp1-sta87.ywp7d.pw",
+      "kcptun" : {
+        "nocomp" : false,
+        "key" : "it's a secrect",
+        "crypt" : "aes",
+        "datashard" : 10,
+        "mtu" : 1350,
+        "mode" : "fast",
+        "parityshard" : 3,
+        "arguments" : ""
+      },
+      "enabled_kcptun" : false,
+      "server_port" : 17346,
+      "remarks_base64" : "5pel5pysMS41"
+    }
+  ],
+  "proxyPort" : 0,
+  "randomAlgorithm" : 0,
+  "proxyEnable" : false,
+  "enabled" : true,
+  "autoban" : false,
+  "proxyAuthUser" : null,
+  "shareOverLan" : false,
+  "localPort" : 1080
+}
